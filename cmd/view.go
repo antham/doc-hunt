8534ba37d03e5ba1271299715eac2b95533c76fe
antham <hamonanth@gmail.com>
2016-07-06 00:01:36 +0200

    ref(cmd/view) : simplify code

--- cmd/view.go
+++ cmd/view.go
@@ -34,27 +34,10 @@ func renderCheck(list *map[file.Doc]map[file.ItemStatus]map[string]bool) {
 	output := ""
 
 	for doc, status := range *list {
-		if len(status[file.IDELETED]) != 0 || len(status[file.IUPDATED]) != 0 || len(status[file.IFAILED]) != 0 || len(status[file.IADDED]) != 0 {
+		if isCheckRenderable(&status) {
 			output += color.CyanString(doc.Identifier + "\n")
 
-			for _, s := range []file.ItemStatus{file.IADDED, file.IUPDATED, file.IDELETED} {
-				if len(status[s]) > 0 {
-					switch s {
-					case file.IADDED:
-						output += fmt.Sprintf("\n  %s \n\n", color.GreenString("Added"))
-					case file.IUPDATED:
-						output += fmt.Sprintf("\n  %s \n\n", color.YellowString("Updated"))
-					case file.IFAILED:
-						output += fmt.Sprintf("\n  %s \n\n", color.RedString("An error occured"))
-					case file.IDELETED:
-						output += fmt.Sprintf("\n  %s \n\n", color.RedString("Deleted"))
-					}
-
-					for filename := range status[s] {
-						output += fmt.Sprintf("    => %s\n", filename)
-					}
-				}
-			}
+			output += renderStatus(&status)
 
 			output += color.MagentaString("----\n")
 		}
@@ -63,4 +46,36 @@ func renderCheck(list *map[file.Doc]map[file.ItemStatus]map[string]bool) {
 	color.Magenta("----")
 
 	fmt.Print(output)
+}
+
+func isCheckRenderable(status *map[file.ItemStatus]map[string]bool) bool {
+	return (len((*status)[file.IDELETED]) != 0 || len((*status)[file.IUPDATED]) != 0 || len((*status)[file.IFAILED]) != 0 || len((*status)[file.IADDED]) != 0)
+}
+
+func renderStatus(status *map[file.ItemStatus]map[string]bool) string {
+	var output string
+
+	for _, s := range []file.ItemStatus{file.IADDED, file.IUPDATED, file.IDELETED} {
+		if len((*status)[s]) == 0 {
+			continue
+		}
+
+		switch s {
+		case file.IADDED:
+			output += fmt.Sprintf("\n  %s \n\n", color.GreenString("Added"))
+		case file.IUPDATED:
+			output += fmt.Sprintf("\n  %s \n\n", color.YellowString("Updated"))
+		case file.IFAILED:
+			output += fmt.Sprintf("\n  %s \n\n", color.RedString("An error occured"))
+		case file.IDELETED:
+			output += fmt.Sprintf("\n  %s \n\n", color.RedString("Deleted"))
+		}
+
+		for filename := range (*status)[s] {
+			output += fmt.Sprintf("    => %s\n", filename)
+		}
+
+	}
+
+	return output
 }
