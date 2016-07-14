630b104d709d2ed563b70630d11e116d490730d5
antham <hamonanth@gmail.com>
2016-07-14 18:22:16 +0200

    fix(cmd/prompt) : remove unused stmt

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -2,15 +2,12 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/chzyer/readline"
 
 	"github.com/antham/doc-hunt/ui"
 	"github.com/antham/doc-hunt/util"
 )
-
-type checker func(line string) error
 
 type terminalReader interface {
 	Readline() (string, error)
@@ -28,70 +25,5 @@ func init() {
 		ui.Error(fmt.Errorf("Something went wrong when initializing prompt"))
 
 		util.ErrorExit()
-	}
-}
-
-var basePrompt = func(prompt string, callback checker) string {
-	fmt.Print(prompt + "\n")
-
-	defer func() {
-		if err := rl.Close(); err != nil {
-			ui.Error(err)
-
-			util.ErrorExit()
-		}
-	}()
-
-	for {
-		line, err := rl.Readline()
-
-		if err != nil {
-			ui.Error(err)
-
-			util.ErrorExit()
-		}
-
-		err = callback(line)
-
-		if err != nil {
-			ui.Error(err)
-
-			continue
-		}
-
-		return line
-	}
-}
-
-var globalPrompt = func(filename string, deleted *map[string]bool, moved *map[string]string) checker {
-	return func(line string) error {
-		if line == "d" {
-			(*deleted)[filename] = true
-
-			return nil
-		}
-
-		if line == "r" {
-			basePrompt(fmt.Sprintf(`Write an existing file name to rename "%s"`, filename), renamePrompt(filename, moved))
-
-			return nil
-		}
-
-		return fmt.Errorf(`This action "%s" : doesn't exist`, line)
-	}
-}
-
-var renamePrompt = func(path string, moved *map[string]string) checker {
-	return func(line string) error {
-		filename := util.GetAbsPath(line)
-		_, err := os.Stat(filename)
-
-		if err != nil && !os.IsExist(err) {
-			return fmt.Errorf(`File "%s" doesn't exist, please enter an existing filename`, line)
-		}
-
-		(*moved)[path] = line
-
-		return nil
 	}
 }
--- cmd/prompt_test.go
+++ cmd/prompt_test.go
@@ -1,11 +1,5 @@
 package cmd
 
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
 type mockTerminalReader struct {
 	output string
 	err    error
@@ -21,86 +15,4 @@ func (m *mockTerminalReader) Readline() (string, error) {
 
 func (m *mockTerminalReader) Close() error {
 	return nil
-}
-
-func TestBasePrompt(t *testing.T) {
-	rl = &mockTerminalReader{"test", nil}
-
-	callback := func(line string) error {
-		return nil
-	}
-
-	assert.Equal(t, "test", basePrompt("A prompt", callback), "Must return data set on prompt")
-}
-
-func TestGlobalPromptDeleteOption(t *testing.T) {
-	rl = &mockTerminalReader{"test", nil}
-
-	filename := "test"
-	deleted := map[string]bool{}
-	moved := map[string]string{}
-
-	err := globalPrompt(filename, &deleted, &moved)("d")
-
-	assert.NoError(t, err, "Must throws no errors")
-	assert.Equal(t, map[string]bool{"test": true}, deleted, "Must stores filename")
-}
-
-func TestGlobalPromptRenameOption(t *testing.T) {
-	bckPrompt := basePrompt
-
-	basePrompt = func(prompt string, callback checker) string {
-		return ""
-	}
-
-	rl = &mockTerminalReader{"test", nil}
-
-	filename := "test"
-	deleted := map[string]bool{}
-	moved := map[string]string{}
-
-	err := globalPrompt(filename, &deleted, &moved)("r")
-
-	assert.NoError(t, err, "Must throws no errors")
-
-	basePrompt = bckPrompt
-}
-
-func TestGlobalPromptWrongOption(t *testing.T) {
-	rl = &mockTerminalReader{"test", nil}
-
-	filename := "test"
-	deleted := map[string]bool{}
-	moved := map[string]string{}
-
-	err := globalPrompt(filename, &deleted, &moved)("w")
-
-	assert.Error(t, err, "Must throws an error")
-	assert.EqualError(t, err, `This action "w" : doesn't exist`, "Must throws an error when option is invalid")
-}
-
-func TestRenamePromptUnexistingFilename(t *testing.T) {
-	rl = &mockTerminalReader{"test", nil}
-
-	filename := "test"
-	moved := map[string]string{}
-
-	err := renamePrompt(filename, &moved)("/tmp/whatever")
-
-	assert.Error(t, err, "Must throws an error")
-	assert.EqualError(t, err, `File "/tmp/whatever" doesn't exist, please enter an existing filename`, "Must throws an error when filename doesn't exist")
-}
-
-func TestRenamePrompt(t *testing.T) {
-	createMocks()
-
-	rl = &mockTerminalReader{"test", nil}
-
-	filename := "test"
-	moved := map[string]string{}
-
-	err := renamePrompt(filename, &moved)("source1.php")
-
-	assert.NoError(t, err, "Must throws no error")
-	assert.Equal(t, map[string]string{"test": "source1.php"}, moved, "Must store original and renamed file")
 }
