3e9e5c5a731a6839db1f41c5274e2e955764c6ac
antham <hamonanth@gmail.com>
2016-08-29 23:04:01 +0200

    ref(ParseIdentifier) : move to util

--- file/util.go
+++ file/util.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 
+	"github.com/glenn-brown/golang-pkg-pcre/src/pkg/pcre"
+
 	"github.com/antham/doc-hunt/util"
 )
 
@@ -20,4 +22,15 @@ func calculateFingerprint(filepath string) (string, error) {
 
 func hasChanged(actualFileSum string, backupFileSum string) bool {
 	return actualFileSum != backupFileSum[:]
+}
+
+// ParseIdentifier extract identifier and category from string
+func ParseIdentifier(value string) (string, SourceCategory) {
+	_, err := pcre.Compile(value, pcre.ANCHORED)
+
+	if err == nil {
+		return value, SFILEREG
+	}
+
+	return value, SERROR
 }
